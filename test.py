45885ba19296ea3cb50a6c45f343c164b67ec3d7
DungTranManh <manhdung5801@gmail.com>
2022-07-22 20:27:01 +0700

    update to 1M components

--- test.py
+++ test.py
@@ -5,41 +5,43 @@ from tracemalloc import start
 import psutil
 
 ram1 = psutil.virtual_memory()[3]
-f = open('convertcsv.json')
-start_time = time.time()
+f = open('convertcsv1M.json')
+# start_time = time.time()
 data = json.load(f)
 
 # database
-connection = sqlite3.connect('data.db')
+connection = sqlite3.connect('data1.db')
 cursor = connection.cursor()
-# cursor.execute("create table name_table (id INTEGER PRIMARY KEY AUTOINCREMENT ,name text)")
+# cursor.execute("create table name_table (id INTEGER PRIMARY KEY AUTOINCREMENT ,name text, age INTERGER, gender text, city text)")
 
 # for i in range (len(data)):
 #     tmp = data[i]
-#     cursor.execute('insert into name_table (name) VALUES (?)',(tmp.get('name'),))
+#     cursor.execute('insert into name_table (name,age,gender,city) VALUES (?,?,?,?)',(tmp.get('first'),tmp.get('age'),tmp.get('gender'),tmp.get('city')))
 
 #   update phần tử có giá trị đặc biệt (giá trị duy nhất có trong bảng)
-# cursor.execute("UPDATE name_table SET name = ? WHERE id = 50000", ("Dung",)) => done
-# cursor.execute("UPDATE name_table SET name = ? WHERE id = 100000", ("Thanh",)) => done
+# cursor.execute("UPDATE name_table SET name = ? WHERE id = 750345", ("Minh",))
+# cursor.execute("UPDATE name_table SET name = ? WHERE id = 1000000", ("Linh",))
 
+start_time = time.time()
 #testing
 #   Phan tu dau database
 # cursor.execute("SELECT * FROM name_table WHERE name = 'Mary'")
 # print(cursor.fetchone())
 
 #   Phan tu nam giua database
-cursor.execute("SELECT * FROM name_table WHERE name = 'Dung'")
-print(cursor.fetchall())
-#   Phan tu nam cuoi database
-# cursor.execute("SELECT * FROM name_table WHERE name = 'Mary'")
-# print(cursor.fetchone())
+# cursor.execute("SELECT * FROM name_table WHERE name = 'Dung'")
+# print(cursor.fetchall())
 
+#   Phan tu nam cuoi database
+cursor.execute("SELECT * FROM name_table WHERE id = 1000000")
+print(cursor.fetchall())
+print("---------Time: %s seconds---------" % (time.time() - start_time))
 # connection.commit()
 connection.close()
 
 # hien thi time run code
-print("---------Time: %s seconds---------" % (time.time() - start_time))
+
 #hien thi cpu usage
 # print(psutil.cpu_percent(time.time() - start_time))
 #hien thi ram usage
-print((psutil.virtual_memory()[3]-ram1)/1e6)
+# print((psutil.virtual_memory()[3]-ram1)/1e6)
